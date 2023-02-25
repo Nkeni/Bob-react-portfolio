1439d4c21838ce192dcce1656a7d3c0003c7db31
Agatha A Msola <agatha@aims.ac.tz>
2023-02-25 20:31:33 +0100

    project component updating

--- src/MainComponent/Projects/Projects.jsx
+++ src/MainComponent/Projects/Projects.jsx
@@ -1,50 +1,78 @@
 import "./Projects.scss";
 import { Row, Container, Card, CardGroup, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import pic from "../../images/lab.jpg";
-import pic1 from "../../images/img1.jpg";
+import pic from "../../images/STM.png";
+import pic1 from "../../images/figureAFM.png";
+import pic2 from "../../images/kpfmfigure.png";
 import paper1 from "../../../src/publication1.pdf";
 
 function Projects() {
   return (
     <Container>
       <Row className="projects">
-        <Col xl="3" className="project">
-          <div className="project-pic1"></div>
+        <Col xl="9">
+          <div className="project-pic">
+            <img src={pic} alt="paper1" />
+          </div>
+        </Col>
+        <Col xl="3">
+          {" "}
           <div className="description">
             <p>
               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi,
               aliquid?
             </p>
-          </div>
-          <div>
-            <a href={paper1} target="blank" rel="noreferrer">
-              <Button variant="outline-dark">Pdf</Button>{" "}
-            </a>
-          </div>
-        </Col>
 
-        <Col xl="3" className="project">
-          <div className="project-pic2"></div>
-          <div className="description">
-            <p>Coming soon...</p>
-          </div>
-          <div>
-            <Button variant="outline-dark">Pdf</Button>{" "}
-          </div>
-        </Col>
-
-        <Col xl="3" className="project">
-          <div className="project-pic3"></div>
-          <div className="description">
-            <p>Coming soon...</p>
-          </div>
-          <div>
-            <Button variant="outline-dark">Pdf</Button>{" "}
+            <div>
+              <a href={paper1} target="blank" rel="noreferrer">
+                <Button variant="outline-dark">Pdf</Button>{" "}
+              </a>
+            </div>
           </div>
         </Col>
       </Row>
       <hr />
+      <Row className="projects">
+        <Col xl="9">
+          <div className="project-pic">
+            <img src={pic1} alt="paper2" />
+          </div>
+        </Col>
+        <Col xl="3">
+          {" "}
+          <div className="description">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi,
+              aliquid?
+            </p>
+
+            <div>
+              <Button variant="outline-dark">Pdf</Button>{" "}
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <hr />
+      <Row className="projects">
+        <Col xl="9">
+          <div className="project-pic">
+            <img src={pic2} alt="paper2" />
+          </div>
+        </Col>
+        <Col xl="3">
+          {" "}
+          <div className="description">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi,
+              aliquid?
+            </p>
+
+            <div>
+              <Button variant="outline-dark">Pdf</Button>{" "}
+            </div>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 }
